fed733265501ef7fcb9294a09777b8cfebb782e6
AdhvikVithun <tadhvik@karunya.edu.in>
2024-01-03 03:54:59 +0530

    Version 5 Created picture dataset for siamese

--- Algorithm.py
+++ Algorithm.py
@@ -11,6 +11,9 @@ import mimetypes
 import time
 import multiprocessing
 import matplotlib.pyplot as plt
+import rarfile
+import shutil
+from PIL import Image
 
 def fuzzy_match(file_name1, file_name2, threshold):
     return fuzz.ratio(file_name1, file_name2) > threshold
@@ -32,6 +35,17 @@ def convert_size(size_in_bytes):
     else:
         return f"{size_in_bytes} bytes"
 
+def is_image(file_path):
+    image_extensions = {'.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tiff', '.webp'}
+    _, extension = os.path.splitext(file_path)
+    return extension.lower() in image_extensions
+
+def is_video(file_path):
+    video_extensions = {'.mp4', '.avi', '.mkv', '.mov', '.flv', '.wmv', '.mpeg', '.3gp'}
+    _, extension = os.path.splitext(file_path)
+    return extension.lower() in video_extensions
+
+
 def extract_archive(archive_path, extract_folder):
     _, extension = os.path.splitext(archive_path)
 
@@ -44,6 +58,9 @@ def extract_archive(archive_path, extract_folder):
     elif extension.lower() == '.gz':
         with zipfile.ZipFile(archive_path, 'r') as zip_ref:
             zip_ref.extractall(extract_folder)
+    elif extension.lower() == '.rar':
+        with rarfile.RarFile(archive_path, 'r') as rar:
+            rar.extractall(path=extract_folder)
 
 def hash_file(file_path):
     hasher = hashlib.md5()
@@ -53,35 +70,42 @@ def hash_file(file_path):
     return hasher.hexdigest()
 
 def explore_and_find_duplicates(base_path):
-    folder_times = defaultdict(float)  # To store time taken for each folder
+    folder_times = defaultdict(float)
     unique_file_types = defaultdict(lambda: {'types': set(), 'size': 0})
     all_file_info = {}
     exact_duplicates = defaultdict(set)
     fuzzy_duplicates = defaultdict(set)
-    metadata_info = set()  # Use a set to store unique metadata information
+    metadata_info = set()
     lock = threading.Lock()
 
     def process_files(folder_name, file_paths):
         local_unique_file_types = {}
-        local_metadata_info = set()  # Store metadata information for this run
+        local_metadata_info = set()
+
         for file_path in file_paths:
             if is_considered_file(file_path):
-                start_time = time.time()  # Record start time for each file
+                start_time = time.time()
                 file_type, file_size, mime_type, mime_encoding = get_file_info(file_path)
+
+                if is_image(file_path):
+                    image_dataset_path = "D:\\adhvik\\adh\\Hackathon\\space hack\\Data RR\\hack code\\picture dataset"
+                    shutil.copy(file_path, os.path.join(image_dataset_path, os.path.basename(file_path)))
+                elif is_video(file_path):
+                    video_dataset_path = "D:\\adhvik\\adh\\Hackathon\\space hack\\Data RR\\hack code\\picture dataset"
+                    shutil.copy(file_path, os.path.join(video_dataset_path, os.path.basename(file_path)))
+
                 if file_path not in local_unique_file_types:
                     local_unique_file_types[file_path] = {'types': set(), 'size': 0}
                 local_unique_file_types[file_path]['types'].add(file_type)
                 local_unique_file_types[file_path]['size'] += file_size
 
-                # Store file info for later duplicate check
                 all_file_info[file_path] = {'types': set(), 'size': 0}
                 all_file_info[file_path]['types'].add(file_type)
                 all_file_info[file_path]['size'] += file_size
 
-                # Collect metadata information
                 local_metadata_info.add((file_path, os.path.basename(file_path), file_type, convert_size(file_size), mime_type, mime_encoding))
 
-                end_time = time.time()  # Record end time for each file
+                end_time = time.time()
                 elapsed_time = end_time - start_time
                 folder_times[folder_name] += elapsed_time
 
@@ -92,18 +116,17 @@ def explore_and_find_duplicates(base_path):
                 unique_file_types[file_path]['types'].update(file_info['types'])
                 unique_file_types[file_path]['size'] += file_info['size']
 
-        # Update the global metadata_info with local_metadata_info
         with lock:
             metadata_info.update(local_metadata_info)
 
     def process_folder(folder_path):
         nonlocal unique_file_types
-        start_time = time.time()  # Record start time for each folder
+        start_time = time.time()
         for root, _, files in os.walk(folder_path):
             file_paths = [os.path.join(root, file) for file in files]
             process_files(os.path.basename(root), file_paths)
 
-        end_time = time.time()  # Record end time for each folder
+        end_time = time.time()
         elapsed_time = end_time - start_time
         folder_times[os.path.basename(folder_path)] = elapsed_time
 
@@ -112,17 +135,18 @@ def explore_and_find_duplicates(base_path):
         for file in os.listdir(folder_path):
             file_path = os.path.join(folder_path, file)
 
-            if file.endswith(('.zip', '.tar', '.gz', '.bz2', '.rar', '.7z')):
-                print(f"Exploring contents of: {file_path}")
-                extract_folder = os.path.join(folder_path, os.path.splitext(file)[0])
-                extract_archive(file_path, extract_folder)
-                # Now, process the contents of the extracted folder
-                process_folder(extract_folder)
-            else:
-                # If it's not a compressed file, process it directly
-                process_folder(file_path)
+            try:
+                if file.endswith(('.zip', '.tar', '.gz', '.bz2', '.rar', '.7z')):
+                    print(f"Exploring contents of: {file_path}")
+                    extract_folder = os.path.join(folder_path, os.path.splitext(file)[0])
+                    extract_archive(file_path, extract_folder)
+                    process_folder(extract_folder)
+                else:
+                    process_folder(file_path)
 
-    # Use maximum number of threads
+            except Exception as e:
+                print(f"Error processing {file}: {e}")
+
     num_threads = min(multiprocessing.cpu_count() * 2, 32)
     with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
         futures = [
@@ -130,23 +154,19 @@ def explore_and_find_duplicates(base_path):
             executor.submit(process_folder, base_path)
         ]
 
-        # Wait for threads to complete
         for future in concurrent.futures.as_completed(futures):
             try:
                 future.result()
             except Exception as exc:
                 print(f"Error: {exc}")
 
-        # Ensure all threads are finished before proceeding
         executor.shutdown(wait=True)
 
-    # Parallelize hashing process using maximum number of processes
     with concurrent.futures.ProcessPoolExecutor(max_workers=multiprocessing.cpu_count()) as executor:
         hash_values = list(executor.map(hash_file, all_file_info.keys()))
         all_file_info_hashes = dict(zip(all_file_info.keys(), hash_values))
 
-    # Find common files using fuzzy matching for file name and size
-    processed_files = set()  # To keep track of processed files
+    processed_files = set()
     for file_path1, info1 in all_file_info.items():
         if file_path1 in processed_files:
             continue
@@ -158,7 +178,6 @@ def explore_and_find_duplicates(base_path):
                 fuzzy_match(os.path.basename(file_path1), os.path.basename(file_path2), 99) and
                 info1['size'] == info2['size']
             ):
-                # Exact match
                 current_exact_duplicates.add(file_path2)
                 processed_files.add(file_path2)
             elif (
@@ -166,7 +185,6 @@ def explore_and_find_duplicates(base_path):
                 fuzzy_match(os.path.basename(file_path1), os.path.basename(file_path2), 80) and
                 info1['size'] == info2['size']
             ):
-                # Slightly similar match
                 current_fuzzy_duplicates.add(file_path2)
                 processed_files.add(file_path2)
 
@@ -175,12 +193,10 @@ def explore_and_find_duplicates(base_path):
         if current_fuzzy_duplicates:
             fuzzy_duplicates[file_path1] = current_fuzzy_duplicates
 
-    # Create separate DataFrames for exactly same and slightly similar files
     columns = ['File1', 'File2', 'ContentMatch', 'File1Name', 'File2Name']
     df_exact = pd.DataFrame(columns=columns)
     df_fuzzy = pd.DataFrame(columns=columns)
 
-    # Populate DataFrames
     for file_path1, duplicate_addresses in exact_duplicates.items():
         for file_path2 in duplicate_addresses:
             hash1 = all_file_info_hashes[file_path1]
@@ -207,27 +223,23 @@ def explore_and_find_duplicates(base_path):
                 'ContentMatch': [content_match],
             })], ignore_index=True)
 
-    # Print metadata information
     metadata_df = pd.DataFrame(metadata_info, columns=['File', 'File Name', 'File Type', 'File Size', 'MIME Type', 'MIME Encoding'])
     metadata_excel_filename = "metadata.xlsx"
     metadata_df.to_excel(metadata_excel_filename, index=False)
     print(f"\nMetadata DataFrame saved to {metadata_excel_filename}")
 
-    # Print exactly duplicated files
     print("\nExactly Same Files:")
     if not df_exact.empty:
         print(df_exact)
     else:
         print("No exactly same files found.")
 
-    # Print slightly similar files
     print("\nSlightly Similar Files:")
     if not df_fuzzy.empty:
         print(df_fuzzy)
     else:
         print("No slightly similar files found.")
 
-    # Save the DataFrames to Excel files
     excel_exact_filename = "exact_duplicates.xlsx"
     excel_fuzzy_filename = "fuzzy_duplicates.xlsx"
     siamese_dataset_excel_filename = "siamese_dataset.xlsx"
@@ -235,7 +247,6 @@ def explore_and_find_duplicates(base_path):
     df_exact.to_excel(excel_exact_filename, index=False)
     df_fuzzy.to_excel(excel_fuzzy_filename, index=False)
 
-    # Create Siamese Dataset
     siamese_dataset = pd.DataFrame(columns=['File1Name', 'File2Name', 'HashFile1', 'HashFile2', 'Match'])
 
     for file_path1, duplicate_addresses in exact_duplicates.items():
@@ -264,7 +275,6 @@ def explore_and_find_duplicates(base_path):
                 'Match': [int(content_match)],
             })], ignore_index=True)
 
-
     siamese_dataset.to_excel(siamese_dataset_excel_filename, index=False)
 
     print(f"\nExactly Same Files DataFrame saved to {excel_exact_filename}")
@@ -274,11 +284,8 @@ def explore_and_find_duplicates(base_path):
     end_time2 = time.time()
     elapsed_time2 = end_time2 - start_time2
 
-
-    # Create a bar graph for the time taken for each folder
     folder_names = list(folder_times.keys())
     times = list(folder_times.values())
-
 
     plt.figure(figsize=(15, 8))
     plt.bar(folder_names, times, color='blue')
@@ -289,19 +296,13 @@ def explore_and_find_duplicates(base_path):
     plt.tight_layout()
     plt.show()
 
-    # Calculate the overall elapsed time
     end_time1 = time.time()
     elapsed_time1 = end_time1 - start_time1
 
-    # Print the overall elapsed time
     print(f"\nTotal execution time: {elapsed_time1} seconds")
 
 if __name__ == "__main__":
-    base_path = r"D:\\adhvik\\adh\\Hackathon\\space hack\\top12 data\\topic12\\dataset1"
-    #D:\\adhvik\\adh\\Hackathon\\space hack\\top12 data\\topic12\\dataset1
-    #D:\\adhvik\\adh\\Hackathon\\space hack\\Data RR\\dataset1
-    #C:\\Users\\prath\\Downloads
-    # Start the timer
+    base_path = r"D:\adhvik\adh\Hackathon\space hack\Data RR\data Set\topic12\dataset1"
     start_time1 = time.time()
     start_time2 = time.time()
     print("Timer started")
